b3ab5beaadea1e7069bce919dd3c51e4c910fbe5
Thomas Lamboul <thomas_lamboul@hotmail.fr>
2020-06-17 16:15:30 +0200

    création du routeur dans index.php + deplacement de l'ancien code html de index.php dans list_parners_view.php

--- index.php
+++ index.php
@@ -1,25 +1,44 @@
-<?php $css = 'style.css'; ?>
-<?php $title = 'GBAF - Groupement Banque Assurance Français'; ?>
+<?php
+session_start();
 
-<?php ob_start(); ?>
-    	<section id="section1">
-    		<h1>Le Groupement Banque Assurance Français​</h1>
-    		<p>Représentant de la profession bancaire et des assureurs sur tous les axes de la réglementation financière française, nous gérons près de 80 millions de comptes sur le territoire national. Le GBAF représente les 6 grands groupes français : BNP Paribas, BPCE, Crédit Agricole, Crédit Mutuel-CIC, Société Générale et La Banque Postale. Notre mission est de promouvoir l'activité bancaire à l’échelle nationale. Nous sommes également un interlocuteur privilégié des pouvoirs publics. Nous sommes fiers de vous accueillir sur notre extranet mettant à disposition des ressources pour les salariés des différentes banques de notre groupe.</p>
-    		<img src="">
-    	</section>
-    	<section>
-    		<h2>Nos acteurs et partenaires</h2>
-    		<p>Retrouver toutes les informations sur nos acteurs et partenaires</p>
-    		<div>
-    			<div>
-    				<img src="">
-    				<h3></h3>
-    				<p></p>
-    				<a href=""></a>
-    			</div>
-    		</div>
-    	</section>
+require('controller/frontend.php');
 
-<?php $content = ob_get_clean(); ?>
+try
+{
+	if(isset($_GET['action']) AND $_GET['action'] == 'logout')
+	{
+		logout();
+	}
+	elseif(isset($_GET['action']) AND $_GET['action'] == 'registration')
+	{
 
-<?php require('view/frontend/page_template.php'); ?>
+		registration();
+	}
+	elseif (isset($_SESSION['username']) AND isset($_SESSION['password']))
+	{
+		if(isset($_GET['action']) AND $_GET['action'] == 'comments') 
+		{
+			list_comments();
+		}
+		else
+		{
+			list_partners();
+		}
+	}
+	//Si des cookies existent, on les compare avec la BDD. S'il y a bien une correspondance dans la BDD on effectue la connexion auto
+	elseif (isset($_COOKIE['username']) AND isset($_COOKIE['password']) AND check_cookies($_COOKIE['username'], $_COOKIE['password'])) 
+	{
+			$_SESSION['username'] = $_COOKIE['username'];
+			$_SESSION['password'] = $_COOKIE['password'];
+
+			header('Location: index.php');
+	}
+	else
+	{
+		connection();
+	}
+}
+catch(Exception $e)
+{
+	echo 'Erreur : ' . $e->getMessage();
+}
